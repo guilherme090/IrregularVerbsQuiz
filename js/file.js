bea0fe2d5454206dff591002ba47c2293c85c27a
Guilherme Simoes <guilherme-elec12@hotmail.com>
2020-09-25 03:06:52 -0300

    <feature> Added labels that respond to START QUIZ, CORRECT and INCORRECT buttons.
    Added labels: word number, correct answers.

--- js/file.js
+++ js/file.js
@@ -19,7 +19,20 @@ Labels
 Labels that will store the student's data.
 */ 
 
+// Student data
 
+let studentName = document.querySelector('#student-name');
+let studentLearnedWords = document.querySelector('#learned-words');
+
+// Quiz area
+
+let wordNumber = document.querySelector('#word-no');
+let correctPct = document.querySelector('#correct-pct');
+
+// Update functions
+function updateStudentScore(){
+    correctPct.innerHTML = (aluno.words_right / aluno.words_total * 100).toFixed(2); 
+}
 
 /* 
 ----------------------------------------------------------------------------------
@@ -32,18 +45,30 @@ let newStudentBtn = document.querySelector('#new-student');
 newStudentBtn.onclick = function(){
     console.log("Botão clicado. Novo estudante será adicionado.");
     aluno.name = 'Nome Legal';
+    aluno.words_learned = 20;
     console.log(aluno);
+    studentName.innerHTML = aluno.name;
+    studentLearnedWords.innerHTML = aluno.words_learned;
 };
+
+let startBtn = document.querySelector('#btn-start');
+startBtn.onclick = function(){
+    wordNumber.innerHTML = 1;
+}
 
 let correctBtn = document.querySelector('#btn-correct');
 correctBtn.onclick = function(){
     aluno.words_right ++;
     aluno.words_total ++;
     console.log(aluno);
+    wordNumber.innerHTML++;
+    updateStudentScore();
 };
 
 let incorrectBtn = document.querySelector('#btn-incorrect');
 incorrectBtn.onclick = function(){
     aluno.words_total ++;
     console.log(aluno);
+    wordNumber.innerHTML++;
+    updateStudentScore();
 };
