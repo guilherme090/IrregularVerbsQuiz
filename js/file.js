2b8fce4e4e7c7edf0ec840801c0c0f2477a4f5c2
Guilherme Simoes <guilherme-elec12@hotmail.com>
2020-09-28 15:48:24 -0300

    <fix> changed way in which verbs are shuffled. Instead of list of indices, used key 'taken'.

--- js/file.js
+++ js/file.js
@@ -28,6 +28,9 @@ let studentLearnedWords = document.querySelector('#learned-words');
 
 let wordNumber = document.querySelector('#word-no');
 let correctPct = document.querySelector('#correct-pct');
+let infinitiveAnswer = document.querySelector('#answer-infinitive');
+let pastSimpleAnswer = document.querySelector('#answer-past-simple');
+let pastParticipleAnswer = document.querySelector('#answer-past-participle');
 
 // Update functions
 function updateStudentScore(){
@@ -60,6 +63,7 @@ newStudentBtn.onclick = function(){
 let startBtn = document.querySelector('#btn-start');
 startBtn.onclick = function(){
     wordNumber.innerHTML = 1;
+    resetVerbList(listOfVerbs);
     stateMachine(states.QUIZ_STARTED_NO_ANSWER);
 }
 
@@ -90,7 +94,13 @@ showAnsBtn.onclick = function(){
     stateMachine(states.QUIZ_STARTED_ANSWER_SHOWN);
 };
 
-// File Reading
+/* 
+----------------------------------------------------------------------------------
+File Reading
+----------------------------------------------------------------------------------
+The program reads a .txt file loaded by the user containing information to be 
+stored in data structures (student name and pertaining data).
+*/ 
 
 var txtRead = [];
 
@@ -111,7 +121,13 @@ loadBtn.addEventListener('input', function(e){
     stateMachine(states.STUDENT_REGISTERED);
 });
 
-// File Writing
+/* 
+----------------------------------------------------------------------------------
+File Writing
+----------------------------------------------------------------------------------
+The program writes to a .txt file the last quiz's log as well as the student name
+and pertaining data.
+*/ 
 
 const saveBtn = document.querySelector('#btn-save');
 saveBtn.onclick = function(){
@@ -162,6 +178,9 @@ function stateMachine(currentState){
             saveBtn.disabled = true;
             break;
         case states.QUIZ_STARTED_NO_ANSWER:
+            shuffledIndex = shuffleVerb(listOfVerbs, wordNumber);
+            console.log(shuffledIndex);
+            showInfinitive(infinitiveAnswer, pastSimpleAnswer, pastParticipleAnswer, listOfVerbs, shuffledIndex);
             newStudentBtn.disabled = true;
             loadBtn.disabled = true;
             startBtn.disabled = true;
@@ -171,6 +190,7 @@ function stateMachine(currentState){
             saveBtn.disabled = false;
             break;
         case states.QUIZ_STARTED_ANSWER_SHOWN:
+            showPast(infinitiveAnswer, pastSimpleAnswer, pastParticipleAnswer, listOfVerbs, shuffledIndex);
             newStudentBtn.disabled = true;
             loadBtn.disabled = true;
             startBtn.disabled = true;
@@ -195,19 +215,41 @@ function stateMachine(currentState){
 ----------------------------------------------------------------------------------
 The verbs
 ----------------------------------------------------------------------------------
-Data structure to store the irregular verbs.
+Data structure to store the irregular verbs. This part uses the same approach as
+File Reading to get a .txt file with the list of verbs in the infinitive, past
+simple and past participle.
 */ 
 
 class verb {
-    constructor(infinitive, past_simple, past_participle){
-        this.infinitive = infinitive;
-        this.past_simple = past_simple;
-        this. past_participle = past_participle;
+    constructor(infinitive, past_simple, past_participle, isTaken){
+        this._infinitive = infinitive;
+        this._past_simple = past_simple;
+        this._past_participle = past_participle;
+        this._taken = isTaken;       
+    }
+
+    get infinitive(){
+        return this._infinitive;
+    }
+
+    get pastSimple(){
+        return this._past_simple;
+    }
+
+    get pastParticiple(){
+        return this._past_participle;
+    }
+
+    get taken(){
+        return this._taken;
+    }
+
+    set taken(newTaken){
+        this._taken = newTaken;
     }
 }
 
 const listOfVerbs = []; // stores all verbs that are in the txt file loaded as verb list.
-const listOfIndices = []; // stores only numbers that point to a position in the listOfVerbs. Will lose elements as verbs are sorted.
 
 const loadListBtn = document.querySelector('#word-list');
 loadListBtn.addEventListener('input', function(e){
@@ -219,30 +261,61 @@ loadListBtn.addEventListener('input', function(e){
 
         verbsRead.forEach(verbForm => {
             let singleVerb = verbForm.split('#');
-            listOfVerbs.push(new verb(singleVerb[0], singleVerb[1], singleVerb[2]));   
+            listOfVerbs.push(new verb(singleVerb[0], singleVerb[1], singleVerb[2], false));   
         });
 
         console.log(listOfVerbs);
-        createListOfIndices(listOfVerbs, listOfIndices);
-        console.log(listOfIndices);
     }
     fr2.readAsText(this.files[0]);
 });
 
-function createListOfIndices(currentListOfVerbs, currentListOfIndices){  
-    /*
-    List of verbs is an immutable array, so that the quiz can be played several times without erasing many data structures.
-    So, we create a list containing just indexes that will be taken out as they are shuffled and refilled when quiz must
-    be played again.
-    */
+/*
+----------------------------------------------------------------------------------
+Initializing list of verbs
+----------------------------------------------------------------------------------
+Here, verbs are already inside the program's data structures. The program sets
+the attribute taken to false, meaning that the student has not answered a question
+about that particular verb. When it happens, the value will be set to true.
+*/
 
-    currentListOfIndices.splice(0, currentListOfIndices.length); // erase everything that is on the array.
-
-    // Create list of indices related to the list of verbs.
-    for(let i = 0; i < currentListOfVerbs.length; i++){
-        currentListOfIndices.push(i);
-    }
+function resetVerbList(theListOfVerbs){
+    theListOfVerbs.forEach(
+        function(theVerb){
+            theVerb.taken = false;
+        }
+    );
 }
 
+let shuffledIndex = 0; // Initialize shuffled number
 
+function shuffleVerb(currentListOfVerbs, currentWordCount){
+    // Shuffled number is a number between 0 and the end of the verb array (excluding verbs already taken)
+    // the number of verbs that were already taken is the current wordCount - 1.
+    let takenVerbs = Number(currentWordCount.innerHTML) - 1;
+    console.log('Word count = ' + takenVerbs);
+    let shuffledNumber = Math.floor(Math.random() * (currentListOfVerbs.length - takenVerbs)); 
+    console.log('shuffled number = ' + shuffledNumber);
+    // Scan the whole list of verbs to point to a non-taken verb in sorted position
+    for(let i = 0; i < shuffledNumber || currentListOfVerbs[i].taken === true ; i++){
+        if(currentListOfVerbs[i].taken === true){
+            shuffledNumber++;
+        }
+    }
+    console.log('shuffled index = ' + shuffledNumber);
 
+    // tell list of verbs that this particular verb was taken
+    currentListOfVerbs[shuffledNumber].taken = true;
+    return shuffledNumber;
+}
+
+function showInfinitive(theInfinitive, thePastSimple, thePastParticiple, theListOfVerbs, theIndex){
+    theInfinitive.innerHTML = theListOfVerbs[theIndex].infinitive;
+    thePastSimple.innerHTML = '';
+    thePastParticiple.innerHTML = '';
+}
+
+function showPast(theInfinitive, thePastSimple, thePastParticiple, theListOfVerbs, theIndex){
+    theInfinitive.innerHTML = theListOfVerbs[theIndex].infinitive;
+    thePastSimple.innerHTML = theListOfVerbs[theIndex].pastSimple;
+    thePastParticiple.innerHTML = theListOfVerbs[theIndex].pastParticiple;
+}
